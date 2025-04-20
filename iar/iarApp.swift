9e2cef89fc0e02b023d444e0d529f2eee2af5a37
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2025-04-20 13:47:31 -0400

    fix background task crash on 18.4

--- iar/iarApp.swift
+++ iar/iarApp.swift
@@ -12,53 +12,59 @@ import WidgetKit
 
 @main
 struct iarApp: App {
-    
-    @Environment(\.scenePhase) private var phase
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self)
     var appDelegate
-    
-    let backgroundTaskIdentifier = "com.lemosys.IARMasjid.localNotificationsRefresh"
 
     var body: some Scene {
         WindowGroup {
             MainView()
         }
-        .onChange(of: phase) { newPhase in
-            switch newPhase {
-            case .active:
-                scheduleAppRefresh()
-            default: break
-            }
-        }.backgroundTask(.appRefresh(backgroundTaskIdentifier)) {
-            await scheduleAppRefresh()
+    }
+}
 
-            let prayerProvider = NetworkPrayerProvider()
-            guard let schedule = try? await prayerProvider.fetchPrayers(forceRefresh: false) else {
-                return
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    let backgroundTaskIdentifier = "com.lemosys.IARMasjid.localNotificationsRefresh"
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { bgTask in
+            Task {
+                await self.refreshNotifications()
+                self.scheduleAppRefresh()
+                bgTask.setTaskCompleted(success: true)
             }
-            
-            let notificationSettings = NotificationSettings()
-            let notificationController = NotificationController()
-            let enabledPrayers = Prayer.allCases.filter { notificationSettings.isEnabled(for: $0) }
-                    
-            await notificationController.scheduleNotifications(prayerDays: schedule.prayerDays,
-                                                               enabledPrayers: enabledPrayers,
-                                                               notificationType: notificationSettings.type)
-            WidgetCenter.shared.reloadAllTimelines()
         }
+        OneSignal.initialize("01fcf852-7b3f-4b53-a733-4cb8241bd193", withLaunchOptions: launchOptions)
+        scheduleAppRefresh()
+        return true
     }
     
     func scheduleAppRefresh() {
         let request = BGAppRefreshTaskRequest(identifier: backgroundTaskIdentifier)
         request.earliestBeginDate = Date().addingTimeInterval(60 * 60 * 24)
-        try? BGTaskScheduler.shared.submit(request)
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("Error scheduling background task \(error)")
+        }
     }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        OneSignal.initialize("01fcf852-7b3f-4b53-a733-4cb8241bd193", withLaunchOptions: launchOptions)
-        return true
+    
+    func refreshNotifications() async {
+        let prayerProvider = NetworkPrayerProvider()
+        guard let schedule = try? await prayerProvider.fetchPrayers(forceRefresh: false) else {
+            return
+        }
+        
+        let notificationSettings = NotificationSettings()
+        let notificationController = NotificationController()
+        let enabledPrayers = Prayer.allCases.filter { notificationSettings.isEnabled(for: $0) }
+                
+        await notificationController.scheduleNotifications(prayerDays: schedule.prayerDays,
+                                                           enabledPrayers: enabledPrayers,
+                                                           notificationType: notificationSettings.type)
+        WidgetCenter.shared.reloadAllTimelines()
     }
 }
